cb1f455545805b18293ab09f590db5be89199f5e
naru-hiyoko <narusawa-a@mm.inf.uec.ac.jp>
2017-05-11 17:08:23 +0900

    bug fix

--- python/cgi-bin/mirror.py
+++ python/cgi-bin/mirror.py
@@ -8,23 +8,33 @@ print('Content-type: text/html; charset=UTF-8\n\n')
 
 storage = cgi.FieldStorage()
 
-DEBUG = False
+DEBUG = True
 
-#with open('/dev/ttys003', 'a') as f:
-with open('/dev/null', 'a') as f:
-    id = storage["id"].value.decode('utf-8')
-    image = storage["image"].value
-    _json = storage["json"].value.decode('utf-8')
-    
-    with open(join('./data/img', id + '.jpg'), 'wb') as f2:
-        f2.write(image)
-    
-    with open(join('./data/json', id + '.json'), 'w') as f2:
-        f2.write(_json)
+with open('/dev/ttys002', 'a') as f:
+#with open('/dev/null', 'a') as f:
+    id = storage.getvalue('id', "")
+    id = "" if id == "" else id.decode('utf-8')
+    image = storage.getvalue("image", None)
+    _json = storage.getvalue("json", None)
 
+    if image != None:
+        with open(join('./data/img', id + '.jpg'), 'wb') as f2:
+            f2.write(image)
+
+    if _json != None:
+        with open(join('./data/json', id + '.json'), 'wb') as f2:
+            f2.write(_json)
+            
 
     """ check if works. """
     if DEBUG:
         with open(join('./data/json', id + '.json'), 'r') as f2:
             content = json.load(f2)
-            f.write(text)
+            f.write('\n')
+            """ username """
+            f.write(content['username'] + '\n')
+            """ unique id """
+            f.write(content['id'] + '\n')
+            for datum in content['data']:
+                f.write(str(datum))
+                f.write('\n')
